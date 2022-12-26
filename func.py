d28ae3083978897b00432b7173de3dc432504dbd
unknown <dheepi117@gmail.com>
2022-12-26 12:39:43 +0530

    adding file.

---
+++ func.py
@@ -0,0 +1,37 @@
+#no arguement no return type
+def dheep():
+    print("dheepika")
+dheep()
+#with arguement no return type
+def dheep(a,b):
+    print( a+b)
+dheep(2,3)
+
+#with arguement with return type
+def dheep(a,b):
+    return a+b
+a=dheep(2,3)
+print(a)
+
+#no argument with return type
+def dheep():
+    return "dheepika"
+dheep()
+
+
+def dheep(list):
+    for x in list:
+        print(x)
+
+
+list=["mon","tue","wed"]
+dheep(list)
+
+def dheep(list):
+    for x in list:
+        print(x)
+
+
+list=["mon","tue","wed"]
+dheep(list)
+
